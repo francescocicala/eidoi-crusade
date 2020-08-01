29391667e9f957d704c8707a0e59b643bc521077
Francesco <francesco.cicala00@gmail.com>
2020-08-01 10:36:09 +0200

    add

--- src/main/java/francescocicala/Eidos.java
+++ src/main/java/francescocicala/Eidos.java
@@ -1,19 +1,24 @@
 package francescocicala;
 
 public class Eidos {
+	private String name;
     private int attack, defense;
 
-	public Eidos(int attack, int defense) {
+	public Eidos(String name, int attack, int defense) {
+		this.name = name;
         this.attack = attack;
         this.defense = defense;
 	}
 
+	public String getName() {
+		return name;
+	}
 	public Integer getAtt() {
-		return this.attack;
+		return attack;
 	}
 
 	public Integer getDef() {
-		return this.defense;
+		return defense;
 	}
 
 }
--- src/test/java/francescocicala/EidosTest.java
+++ src/test/java/francescocicala/EidosTest.java
@@ -4,17 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class EidosTest {
+    String name = "Sigfried";
+    int attack = 4, defense = 3;
+    Eidos eidos = new Eidos(name, attack, defense);
+
     @Test
-    public void correctAtt(){
-        int attack = 4, defense = 3;
-        Eidos eidos = new Eidos(attack, defense);
+    public void correctName() {
+        assertEquals(name, eidos.getName());
+    }
+    @Test
+    public void correctAtt() {
         assertEquals(attack, eidos.getAtt());
     }
 
     @Test
-    public void correctDef(){
-        int attack = 4, defense = 3;
-        Eidos eidos = new Eidos(attack, defense);
+    public void correctDef() {
         assertEquals(defense, eidos.getDef());
     }
 }
