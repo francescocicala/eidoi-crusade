251a734b8b2b36499895cbbd89dbca4e201a9df1
Francesco <francesco.cicala00@gmail.com>
2020-08-01 11:13:23 +0200

    style change

--- src/main/java/francescocicala/Eidos.java
+++ src/main/java/francescocicala/Eidos.java
@@ -1,22 +1,26 @@
 package francescocicala;
 
 public class Eidos {
-	private String name;
-    private int attack, defense;
+  private String name;
+  private int attack;
+  private int defense;
 
-	public Eidos(String name, int attack, int defense) {
-		this.name = name;
-        this.attack = attack;
-        this.defense = defense;
-	}
-	public String getName() {
-		return name;
-	}
-	public Integer getAtt() {
-		return attack;
-	}
-	public Integer getDef() {
-		return defense;
-	}
+  public Eidos(String name, int attack, int defense) {
+    this.name = name;
+    this.attack = attack;
+    this.defense = defense;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Integer getAtt() {
+    return attack;
+  }
+  
+  public Integer getDef() {
+    return defense;
+  }
 
 }
--- src/test/java/francescocicala/EidosTest.java
+++ src/test/java/francescocicala/EidosTest.java
@@ -1,23 +1,27 @@
 package francescocicala;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class EidosTest {
-    String name = "Sigfried";
-    int attack = 4, defense = 3;
-    Eidos eidos = new Eidos(name, attack, defense);
+  String name = "Sigfried";
+  int attack = 4;
+  int defense = 3;
+  Eidos eidos = new Eidos(name, attack, defense);
 
-    @Test
-    public void correctName() {
-        assertEquals(name, eidos.getName());
-    }
-    @Test
-    public void correctAtt() {
-        assertEquals(attack, eidos.getAtt());
-    }
-    @Test
-    public void correctDef() {
-        assertEquals(defense, eidos.getDef());
-    }
+  @Test
+  public void correctName() {
+    assertEquals(name, eidos.getName());
+  }
+
+  @Test
+  public void correctAtt() {
+    assertEquals(attack, eidos.getAtt());
+  }
+
+  @Test
+  public void correctDef() {
+    assertEquals(defense, eidos.getDef());
+  }
 }
