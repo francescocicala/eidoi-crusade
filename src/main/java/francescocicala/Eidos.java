27f52b1002ad6e2726ef104932c22ffac1daa6eb
Francesco <francesco.cicala00@gmail.com>
2020-08-01 11:25:50 +0200

    add documentation

--- src/main/java/francescocicala/Eidos.java
+++ src/main/java/francescocicala/Eidos.java
@@ -5,20 +5,41 @@ public class Eidos {
   private int attack;
   private int defense;
 
+  /**
+   * Construct an Eidos object.
+   * @param name eidos' name
+   * @param attack eidos' attack value
+   * @param defense eidos' defense value
+   */
   public Eidos(String name, int attack, int defense) {
     this.name = name;
     this.attack = attack;
     this.defense = defense;
   }
 
+  
+  /** 
+   * Returns the name of the eidos.
+   * @return String
+   */
   public String getName() {
     return name;
   }
-
+  
+  
+  /** 
+   * Returns the attack value.
+   * @return Integer
+   */
   public Integer getAtt() {
     return attack;
   }
+
   
+  /** 
+   * Returns the defense value.
+   * @return Integer
+   */
   public Integer getDef() {
     return defense;
   }
