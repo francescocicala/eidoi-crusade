7dabd78f657e1e8f0a1528e243b94519c42e4ca4
Francesco <francesco.cicala00@gmail.com>
2020-08-03 10:01:27 +0200

    add docs

--- src/main/java/francescocicala/EvocationManager.java
+++ src/main/java/francescocicala/EvocationManager.java
@@ -13,6 +13,12 @@ public class EvocationManager {
   private final Integer numberOfDrafts;
   private final String pathToXmlFile;
 
+  /**
+   * Constructs the EvocationManager.
+   * @param pathToXml Relative path to the xml file storing the Eidoi properties.
+   * @param numOfDrafts Number of Eidoi to draw for each Mage. Must be less or equal to the half 
+   *      of the total Eidoi in the xml file (rounded down).
+   */
   public EvocationManager(String pathToXml, Integer numOfDrafts) {
     mages = new Mage[2];
     mages[0] = new Mage();
@@ -21,6 +27,13 @@ public class EvocationManager {
     pathToXmlFile = pathToXml;
   }
 
+  
+  /** 
+   * Starts the Evocation Phase, where every Mage will draw an alliance.
+   * @throws SAXException
+   * @throws IOException
+   * @throws ParserConfigurationException
+   */
   public void startDraft() throws SAXException, IOException, ParserConfigurationException {
     ShuffledDeck deck = new ShuffledDeck(pathToXmlFile);
     for (Integer i = 1; i <= numberOfDrafts; i++) {
@@ -32,10 +45,20 @@ public class EvocationManager {
     }
   }
   
+  
+  /** 
+   * Returns the first Mage.
+   * @return Mage
+   */
   public Mage getMage1() {
     return mages[0];
   }
 
+  
+  /** 
+   * Returns the second Mage.
+   * @return Mage
+   */
   public Mage getMage2() {
     return mages[1];
   }
